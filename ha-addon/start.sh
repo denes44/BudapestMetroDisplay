35dddadabc5d332170826db5a239bf9f54c36796
denes44 <60078357+denes44@users.noreply.github.com>
2024-10-27 15:22:22 +0100

    Moved ha-addon and proxmox-script to the root

--- ha-addon/start.sh
+++ ha-addon/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-# Get the log level from the environment variable
-APP_LOG_LEVEL=$(bashio::config 'APP_LOG_LEVEL')
-
-# Force sACN output to unicast
-SACN_MULTICAST=false
+# Set location for logs
+mkdir -p /config/logs
+export LOG_PATH="/config/logs/"
 
 # Change working directory
 cd /opt/BudapestMetroDisplay/
@@ -12,7 +10,9 @@ cd /opt/BudapestMetroDisplay/
 # Initialize command
 COMMAND="/opt/BudapestMetroDisplay/.venv/bin/BudapestMetroDisplay"
 
-# Check the log level and modify the command accordingly
+# Get the log level from the configuration
+APP_LOG_LEVEL=$(bashio::config 'APP_LOG_LEVEL')
+
 if [[ "$APP_LOG_LEVEL" == "trace" ]]; then
     exec $COMMAND --trace
 elif [[ "$APP_LOG_LEVEL" == "debug" ]]; then
